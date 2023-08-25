234ffbfb6c7b4bf710f45c5a684bfe0ca2b5cd63
Valanh <lanhtv0923@gmail.com>
2023-08-25 22:04:29 +0700

    Push ManActivity.java

--- app/src/main/java/lanhtv/adroid/shop_v/MainActivity.java
+++ app/src/main/java/lanhtv/adroid/shop_v/MainActivity.java
@@ -3,12 +3,17 @@ package lanhtv.adroid.shop_v;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_name = findViewById(R.id.name);
+
+        tv_name.setText("chào bạn ");
     }
 }
