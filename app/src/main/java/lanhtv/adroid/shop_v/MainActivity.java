caa4fa4d623d2d807ad76b1a9ce9fa507e43da68
Valanh <lanhtv0923@gmail.com>
2023-08-25 21:50:04 +0700

    Initial commit

---
+++ app/src/main/java/lanhtv/adroid/shop_v/MainActivity.java
@@ -0,0 +1,14 @@
+package lanhtv.adroid.shop_v;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
