f0dc0744ba7d236a2e3687c0b4cd9507235a1eb3
Valanh <lanhtv0923@gmail.com>
2023-08-29 20:33:35 +0700

    SetOnclick for tv_name (line 20:25)

--- app/src/main/java/lanhtv/adroid/shop_v/MainActivity.java
+++ app/src/main/java/lanhtv/adroid/shop_v/MainActivity.java
@@ -3,11 +3,13 @@ package lanhtv.adroid.shop_v;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
         tv_name = findViewById(R.id.name);
         //comment trên git cập nhật cho file code
         tv_name.setText("chào bạn");
+        tv_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_name.setText("chạm vào tôi làm gì");
+            }
+        });
     }
 }
