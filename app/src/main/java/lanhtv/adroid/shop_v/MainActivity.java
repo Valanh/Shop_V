bfc80d1f5116bb5929079c3a1f63877beedc97f7
Valanh <lanhtv0923@gmail.com>
2023-08-29 20:42:48 +0700

    bắt đầu tạo app

--- app/src/main/java/lanhtv/adroid/shop_v/MainActivity.java
+++ app/src/main/java/lanhtv/adroid/shop_v/MainActivity.java
@@ -8,21 +8,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv_name;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_name = findViewById(R.id.name);
-        //comment trên git cập nhật cho file code
-        tv_name.setText("chào bạn");
-        tv_name.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //set text for tv_name
-                tv_name.setText("chạm vào tôi làm gì");
-            }
-        });
     }
 }
